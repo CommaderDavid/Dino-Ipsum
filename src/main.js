62cffefd507b7cc23d49028e2072da4f4ec2819f
David Boedigheimer <stealth259@gmail.com>
2020-04-15 15:53:21 -0700

    Add edit to html, and start main js.

--- src/main.js
+++ src/main.js
@@ -3,3 +3,17 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import { nameOfFunction } from './';
+
+$(document).ready(function () {
+
+  $("form#guess").submit(function (e) {
+    e.preventDefault():
+
+    const vowel = $("select#vowels").val();
+
+    (aysnc () =>{
+
+    });
+    
+  });
+});
