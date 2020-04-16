5da55901fefe0a8d978717028b6900d9f337cf80
David Boedigheimer <stealth259@gmail.com>
2020-04-16 15:05:20 -0700

    Add a click function to handle the random dino name.

--- src/main.js
+++ src/main.js
@@ -5,15 +5,19 @@ import './styles.css';
 
 $(document).ready(function () {
 
-  (async () => {
-    let response = await fetch(`http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=1&words=1`);
-    let jsonifiedResponse = await response.json();
-    getElements(jsonifiedResponse);
+  $("button#get-name").click(function () {
+
+    (async () => {
+      let response = await fetch(`http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=1&words=1`);
+      let jsonifiedResponse = await response.json();
+      getElements(jsonifiedResponse);
+    })();
+
+    const getElements = function (response) {
+      $('.name').text(`${response}`);
+    };
   });
 
-  const getElements = function (response) {
-    $('.name').text(`${response.main}`);
-  };
 
   $("form#guess").submit(function (e) {
     e.preventDefault();
