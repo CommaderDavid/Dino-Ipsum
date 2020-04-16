609e079ec660f6a6cb10d1c0522b6befd4998c4c
David Boedigheimer <stealth259@gmail.com>
2020-04-16 12:45:50 -0700

    Add and correct the order of main js.

--- src/main.js
+++ src/main.js
@@ -2,18 +2,22 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import { nameOfFunction } from './';
 
 $(document).ready(function () {
 
+  (async () => {
+    let response = await fetch(`http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=1&words=1`);
+    let jsonifiedResponse = await response.json();
+    getElements(jsonifiedResponse);
+  });
+
+  const getElements = function (response) {
+    $('.name').text(`${response.main}`);
+  };
+
   $("form#guess").submit(function (e) {
-    e.preventDefault():
+    e.preventDefault();
 
     const vowel = $("select#vowels").val();
-
-    (aysnc () =>{
-
-    });
-    
   });
 });
