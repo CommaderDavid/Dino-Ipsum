6c7007e3ff50e1cda9ebe24d8ec58e0867fd523c
David Boedigheimer <stealth259@gmail.com>
2020-04-16 16:00:58 -0700

    Add reminder for next days work.

--- src/erase-vowels.js
+++ src/erase-vowels.js
@@ -2,4 +2,6 @@ export class EraseVowels {
   constructor() {
     let vowels = [a, e, i, o, u];
   }
+  // Start here and remember to grab the .replace and,
+  //look at a random number generator for getting a number bewteen 0-4.
 }
