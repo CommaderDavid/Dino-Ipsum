31e7a1c20de5e959a502db43723de5247925d80d
David Boedigheimer <stealth259@gmail.com>
2020-04-20 13:41:24 -0700

    Add test for replacement of vowels.

--- __tests__/dino-ipsum.test.js
+++ __tests__/dino-ipsum.test.js
@@ -1,1 +1,11 @@
 // use "debugger" to look for bugs and broken code.
+import { EraseVowels } from "./../src/erase-vowels.js";
+
+describe('Removing Vowels', () => {
+
+  test('should remove Us from the dinosaur name, and replace them with -', () => {
+    let dino = "Technosaurus";
+    let removeU = dino.replace(/u/g, "-");
+    expect(removeU).toEqual("Technosa-r-s");
+  });
+});
