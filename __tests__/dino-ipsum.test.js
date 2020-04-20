b35308cc384f7e62b758383f1cf7937c0b5e95b0
David Boedigheimer <stealth259@gmail.com>
2020-04-20 16:22:45 -0700

    Remove test and add test for more acurate testing.

--- __tests__/dino-ipsum.test.js
+++ __tests__/dino-ipsum.test.js
@@ -3,9 +3,15 @@ import { EraseVowels } from "./../src/erase-vowels.js";
 
 describe('Removing Vowels', () => {
 
-  test('should remove Us from the dinosaur name, and replace them with -', () => {
-    let dino = "Technosaurus";
-    let removeU = dino.replace(/u/g, "-");
-    expect(removeU).toEqual("Technosa-r-s");
+  // test('should remove Us from the dinosaur name, and replace them with -', () => {
+  //   let dino = "Technosaurus";
+  //   let removeU = dino.replace(/u/gi, "-");
+  //   expect(removeU).toEqual("Technosa-r-s");
+  // });
+
+  test('should replace a vowel with a symbol', () => {
+    let testDino = new EraseVowels("Technosaurus");
+    console.log(testDino.randomVowels());
+    expect(testDino).toEqual(testDino);
   });
 });
