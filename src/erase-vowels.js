7b9b0debb53d6fcc67cacf879081651312d5c6a4
David Boedigheimer <stealth259@gmail.com>
2020-04-20 13:41:06 -0700

    Add a dino input, and added the removal of different vowels.

--- src/erase-vowels.js
+++ src/erase-vowels.js
@@ -1,6 +1,14 @@
 export class EraseVowels {
-  constructor() {
+  constructor(dino) {
     let vowels = [a, e, i, o, u];
+    this.dino = dino;
+  }
+  replacementLetter() {
+    let removeA = this.dino.replace(/a/g, "-");
+    let removeE = this.dino.replace(/e/g, "-");
+    let removeI = this.dino.replace(/i/g, "-");
+    let removeO = this.dino.replace(/o/g, "-");
+    let removeU = this.dino.replace(/u/g, "-");
   }
   // Start here and remember to grab the .replace and,
   //look at a random number generator for getting a number bewteen 0-4.
